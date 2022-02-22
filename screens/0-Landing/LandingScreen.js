7190ded1dc12e576c2d58425abb886abd43f2795
david_venegas <venegasdavidm@gmail.com>
2022-02-21 22:01:47 -0500

    wireframed first 3 screens

---
+++ screens/0-Landing/LandingScreen.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from 'react-native'
+
+const LandingScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>LANDING</Text>
+        </SafeAreaView>
+    )
+}
+
+export default LandingScreen
---
+++ screens/1-Auth/AuthScreen.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from 'react-native'
+
+const AuthScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>AUTH</Text>
+        </SafeAreaView>
+    )
+}
+
+export default AuthScreen
