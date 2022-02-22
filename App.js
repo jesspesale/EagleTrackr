3d8cf2be081389efd1e4889f695d5e77c2b6de78
david_venegas <venegasdavidm@gmail.com>
2022-02-21 21:58:46 -0500

    adding initial screens

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import LandingScreen from './screens/0-Landing/LandingScreen'
+import AuthScreen from './screens/1-Auth/AuthScreen'
+import HomeScreen from './screens/2-Home/HomeScreen'
+import 'react-native-gesture-handler'
 
 export default function App() {
+  const Stack = createStackNavigator()
   return (
-    <View style={styles.container}>
-      <Text>EagleTrackr</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen name='LandingScreen' component={LandingScreen} options={{headerShown: false}}/>
+            <Stack.Screen name='AuthScreen' component={AuthScreen} options={{headerShown: false}}/>
+            <Stack.Screen name='HomeScreen' component={HomeScreen} options={{headerShown: false}}/>
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
